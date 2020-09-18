f52cb245cf427ed5c4bbb104ad91c0cc9f5f9ad5
Taewoo Kim <rxd0414@gmail.com>
2020-09-18 16:59:42 +0900

    Nodejs Basic

---
+++ 02.basic.js
@@ -0,0 +1,52 @@
+// 원 둘레와 면적
+let pi = 3.14159;
+let radius = 5;
+
+/* console.log(`반지름이 ${radius}인 원의 둘레는 ${2*pi*radius}입니다.`);
+console.log(`반지름이 ${radius}인 원의 면적은 ${pi*radius*radius}입니다.`); */
+
+// 문자열 복합대입 연산자
+/* let output = 'Hello';
+output += ' World!';
+output += '숫자도 가능합니다.';
+output += '235';
+console.log(output); */
+
+// 6가지 자료형
+/* console.log(typeof('String'));           //string
+console.log(typeof(273));                //숫자
+console.log(typeof(true));               //불
+console.log(typeof(function(){}));       //함수
+console.log(typeof({}));                 //객체
+console.log(typeof(alpha));              //undefined
+ */
+
+ // 자료형 변환
+/*  console.log(Number('273'));
+ console.log(Number(true));
+ console.log(Number('82.7'));
+
+ console.log(parseInt('273'));
+ console.log(parseInt(true));
+ console.log(parseInt('82.7'))
+
+ console.log(parseFloat('273'));
+ console.log(parseFloat(true));
+ console.log(parseFloat('82.7'));
+ */
+ // 숫자의 문자열 자동변환
+/* console.log(52+273);
+console.log("52"+273);
+console.log(52+"273");
+console.log("52"+"273"); */
+
+// 일치 연산자
+/* console.log(`52 == '52' : ${52 == '52'}`);
+console.log(`52 === '52' : ${52 === '52'}`);
+console.log(`0 == "": ${0 == ""}`);
+console.log(`0 === "":${0 === ""}`); */
+
+// 상수
+const constant = "상수";
+//constant = "Constant";
+console.log(constant);
