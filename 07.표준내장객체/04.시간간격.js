5b85026de5d2b58d7ac7c8347d47ffa8b5495c81
Taewoo Kim <rxd0414@gmail.com>
2020-09-23 15:10:33 +0900

    표준내장객체

--- 07.표준내장객체/04.시간간격.js
+++ 07.표준내장객체/04.시간간격.js
@@ -2,4 +2,4 @@ let today = new Date(2020,9,23);
 let examDay = new Date(2020,12,3);
 let diff = examDay.getTime() - today.getTime();
 let dDay = diff/(1000*60*60*24);
-console.log(`오늘은 수능 D-${dDay} 일 입니다`);
+console.log(`오늘은 수능 D-${dDay} 일 입니다.`);
